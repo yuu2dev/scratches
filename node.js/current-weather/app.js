3da5d5fce6f58e4a8c38d47fc198f1bfc636d44a
yu <yuu2dev@gmail.com>
2021-08-21 00:37:19 +0900

    Feat: 주소별 날씨 > weatherstack 으로 날씨 정보 출력

--- node.js/current-weather/app.js
+++ node.js/current-weather/app.js
@@ -5,16 +5,19 @@ const axios = require('axios');
  * 간단한 날씨 알아보기 앱
  */
 
-// 2번째는 주소로 함
+// 2번째 인자는 영문주소 (ex. tong-yeong, busan ...)
 const address = process.argv[2];
+
+const mapboxKEY = null;
+const mapboxAPI = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json`;
+const weatherstackKEY = null;
+const weatherstackAPI = 'http://api.weatherstack.com/current';
 
 if (address === undefined) {
     console.error('주소가 입력되지 않았습니다.', address);
     return;
 }
 
-const mapboxKEY = null;
-const mapboxAPI = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json`;
 const get = async () => {
     try {
         const mapbox = await axios.get(mapboxAPI, {
@@ -27,9 +30,21 @@ const get = async () => {
         feature = mapbox.data.features.shift();
         let lon = feature.center[0];
         let lat = feature.center[1];
-
+        
+        const weatherstack = await axios.get(weatherstackAPI, {
+            params: {
+                access_key: weatherstackKEY,
+                query: `${lat},${lon}`,
+                units: 'm',
+            }
+        });
+        console.info(weatherstack.data);
+        return Promise.resolve({
+            location: feature,
+            weather: weatherstack.data
+        });
     } catch (error) {
-        console.log(error.message);
+        return Promise.reject(error.message);
     }
 }
 
