a012ec51df6a83b2680ac938af7b49b7bc0270f6
minsang8332 <minsang8332@gmail.com>
2024-05-25 22:35:52 +0900

    feat: javascript 개선버블정렬, 삽입정렬

--- js/algorithm/sorting.js
+++ js/algorithm/sorting.js
@@ -35,6 +35,37 @@ const bubble = (arr = []) => {
     }
     return arr
 }
+/** 버블 정렬 개선 - 이미 자리바꿨으면 스킵 */
+const bubble_renewal = (arr = []) => {
+    for (let i=0; i<arr.length-1; i++) {
+        // 정렬되었다고 가정한다
+        let sorted = true
+        for (j=0; j<arr.length-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                const tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
+                // 자리 바꿈이 발생하면 정렬 되지 않은 상태라고 간주
+                sorted = false
+            }
+        }
+        if (sorted == true) {
+            break
+        }
+    }
+    return arr
+}
+/** 삽입 정렬 */
+const insertion = (arr = []) => {
+    for (let i=1; i < arr.length; i++) {
+        let value = arr[i]
+        for (j=i; j > 0 && arr[j-1] > value; j--) {
+            arr[j] = arr[j-1]
+        }
+        arr[j]= value
+    }
+    return arr
+}
 console.log(
     '선택정렬', 
     selection([30, 20, 40, 35, 5, 10, 45, 50, 25, 15])
@@ -43,4 +74,11 @@ console.log(
     '버블정렬',
     bubble([30, 20, 40, 35, 5, 10, 45, 50, 25, 15])
 )
-
+console.log(
+    '버블정렬 개선',
+    bubble([30, 20, 40, 35, 5, 10, 45, 50, 25, 15])
+)
+console.log(
+    '삽입정렬',
+    insertion([30, 20, 40, 35, 5, 10, 45, 50, 25, 15])
+)
